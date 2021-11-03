f77fd52f5f4bdc239427f5030a6fc8fe95abc5c5
Edudubeux <89784543+Edudubeux@users.noreply.github.com>
2021-11-03 14:53:49 -0300

    Angular

---
+++ data.js
@@ -0,0 +1,23 @@
+[
+  {
+    "name": "Eduardo",
+    "phone": "999525480",
+    "color": "blue",
+    "date": new Date(),
+    "op": "Tim"
+  },
+  {
+    "name": "Luca",
+    "phone": "024242469",
+    "color": "green",
+    "date": new Date(),
+    "op": "Oi"
+  },
+  {
+    "name": "Benta",
+    "phone": "069692424",
+    "color": "rgb(218, 54, 182)",
+    "date": new Date(),
+    "op": "GVT"
+  }
+]
