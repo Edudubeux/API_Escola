351af5643d865f42427712d90193eb77e0f00d6c
Edudubeux <eduardodubeux1a@gmail.com>
2021-11-19 18:42:14 -0300

    templateUrl

--- backAndFront/directives/uiAlertDirective.js
+++ backAndFront/directives/uiAlertDirective.js
@@ -1,6 +1,12 @@
 angular.module("listaTelefonica").directive("uiAlert", function () {
     return {
-        templateUrl: "view/alert.html",
+        templateUrl: "../views/alert.html",
         replace: true,
+        restrict: "",
+        //existem 4 tipos de restrict: 
+        // A- diretiva restrita ao atributo do elemento  
+        // E- diretiva restrita ao elemento 
+        // C- diretiva restrita a classe do elemento 
+        // M - diretiva restrita ao comentario do elemento
     }
 })
